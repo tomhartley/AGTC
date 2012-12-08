e2eddf7069eec58d22404963d42ba52290a1f5f2
Tom Hartley <tomhartley@rocketmail.com>
2012-12-08 20:58:15 +0000

    Initial Commit

---
+++ AGTC/THAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  THAppDelegate.h
+//  AGTC
+//
+//  Created by Tom Hartley on 08/12/12.
+//  Copyright (c) 2012 Tom Hartley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class THViewController;
+
+@interface THAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) THViewController *viewController;
+
+@end
---
+++ AGTC/THViewController.h
@@ -0,0 +1,13 @@
+//
+//  THViewController.h
+//  AGTC
+//
+//  Created by Tom Hartley on 08/12/12.
+//  Copyright (c) 2012 Tom Hartley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THViewController : UIViewController
+
+@end
